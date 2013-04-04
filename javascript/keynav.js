6f408023bd01d46cacb44d0c7bfc50195e0e4fd5
Nick <nick@firedev.com>
2013-04-04 09:53:52 +0700

    it works

--- javascript/keynav.js
+++ javascript/keynav.js
@@ -1,12 +1,15 @@
-(function($, window, document, undefined) {
-    var $window = $(window);
+;(function($, window, document, undefined) {
 
-    $.fn.keynav = function(options) {
+    $.fn.keynav = function(checkNav) {
     	var elements = this;
-    	var matrix = [];
-    	var current=$(this).find('.selected');
-    	console.log(current);
-    	current.css({fontSize: '100px'});
+    	var matrix;
+    	var x;
+    	var y;
+    	var current = this.filter('.selected');
+    	var keyNavigationDisabled=false;
+    	if (current.length == 0) current = this.first();
+
+    	current.addClass('selected');
 
 	    function update() {
 	    	var i=0;
@@ -30,14 +33,47 @@
 	    			row[j]=this;
 	    		}
 	    	});
-	    	i++;
 	    	m[i]=row;
 	    	matrix = m;
-	    	console.log(matrix);
+	    	coordinates=findCurrent();
+	    	x=coordinates[0];
+	    	y=coordinates[1];
 	    	return matrix;
 	    }
 
-	    $window.bind("resize", function(event) {
+	    function findCurrent() {
+	    	i=0; j=0; found = false;
+	    	try {
+		    	for (i=0; i<matrix.length; i++) {
+		    		row=matrix[i];
+		    		for (j=0; j<row.length; j++) {
+		    			if (current[0] == row[j]) {
+		    				throw([i,j]);
+		    			}
+
+		    		}
+		    	}
+	    	}
+	    	catch (arr)
+	    	{
+	    		found = [i,j]
+	    	}
+	    	return(found);
+	    }
+
+	    function setCurrent(i,j) {
+	    	if (i<0) i=(matrix.length-1);
+	    	if (i>=matrix.length) i=0;
+	    	if (j<0) j=(matrix[i].length-1);
+	    	if (j>=matrix[i].length) j=0;
+	    	current.removeClass('selected');
+	    	current = $(matrix[i][j]);
+	    	current.addClass('selected');
+	    	x=i;
+	    	y=j;
+	    }
+
+	    $(window).bind("resize", function(event) {
             update();
         });
 
@@ -45,11 +81,31 @@
             update();
         });
 
+
+        $(document).keydown(function(e){
+        	if (checkNav && checkNav()) return;
+            if (e.keyCode == 37) {
+            	// left
+            	setCurrent(x,y-1);
+
+            } else if (e.keyCode == 38) {
+            	// up
+            	setCurrent(x-1,y);
+
+            } else if (e.keyCode == 39) {
+            	// right
+            	setCurrent(x,y+1);
+
+            } else if (e.keyCode == 40) {
+            	// down
+            	setCurrent(x+1,y);
+            } else if (e.keyCode == 13) {
+            	window.location = current.attr('href');
+            }
+        });
+
+
         return this;
 	  }
 
   })(jQuery, window, document);
-
-$(function(){
-  $('.nav_holder a').keynav();
-});
